c6ff1e42a22b8a1256e8e372553063cba6f90738
bharathjinka09 <bharathjinka09@gmail.com>
2020-10-02 11:55:22 +0530

    angular-five-star-rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-five-star-rating';
+  currentRate = 1;
+  rating: any;
+
+  constructor() {
+    this.currentRate = JSON.parse(localStorage.getItem('rating'));
+  }
+
+  storeRating() {
+    this.rating = localStorage.setItem(
+      'rating',
+      JSON.stringify(this.currentRate)
+    );
+  }
 }
